dd71310bbbe568345d5fd5d0c5c152a6ea7e6c5d
Gediminas Skvarnavicius <gediminas.skvarnavicius@gmail.com>
2023-11-06 14:01:20 +0200

    credit card features up to installments

--- auxiliary/eda_functions.py
+++ auxiliary/eda_functions.py
@@ -6,66 +6,6 @@ from sklearn.model_selection import StratifiedKFold
 from IPython.display import Markdown
 from tabulate import tabulate
 
-# def test_with_catboost(
-#     data: pl.DataFrame, data_val: pl.DataFrame, cat_features, sample_size=None
-# ):
-#     # Wether to sample
-#     if sample_size:
-#         training_sample = data.sample(sample_size, shuffle=True, seed=1)
-#         val_sample = data_val.sample(int(sample_size / 3), shuffle=True, seed=1)
-#     else:
-#         training_sample = data
-#         val_sample = data_val
-#     # Filling nulls
-#     for col in cat_features:
-#         training_sample = training_sample.with_columns(
-#             pl.col(col).fill_null("None").alias(col)
-#         )
-#         val_sample = val_sample.with_columns(pl.col(col).fill_null("None").alias(col))
-#     for col in [col for col in training_sample.columns if col not in cat_features]:
-#         training_sample = training_sample.with_columns(
-#             pl.col(col).fill_null(-1).alias(col)
-#         )
-#         val_sample = val_sample.with_columns(pl.col(col).fill_null(-1).alias(col))
-#     # Specifying X and y
-#     X_train = training_sample.drop(columns=["TARGET", "SK_ID_CURR"])
-#     y_train = training_sample["TARGET"]
-
-#     X_val = val_sample.drop(columns=["TARGET", "SK_ID_CURR"])
-#     y_val = val_sample["TARGET"]
-
-#     # Categorical cols
-#     cat_indices = []
-
-#     for col in cat_features:
-#         cat_indices.append(X_train.find_idx_by_name(col))
-#     # Model
-#     cat = CatBoostClassifier(
-#         cat_features=cat_indices,
-#         random_seed=1,
-#         l2_leaf_reg=10,
-#         auto_class_weights="Balanced",
-#     )
-
-#     # Fitting and evaluation
-#     cat.fit(X_train.to_numpy(), y_train.to_numpy(), verbose=0)
-#     preds = cat.predict(X_val.to_numpy())
-#     preds_proba = cat.predict_proba(X_val.to_numpy())
-
-#     initial_importances = pl.DataFrame(
-#         {
-#             "feature": X_train.columns,
-#             "importance": cat.feature_importances_,
-#         }
-#     ).sort("importance", descending=True)
-
-#     # Preparing Output
-#     outputs = {}
-#     outputs["report"] = classification_report(y_val, preds)
-#     outputs["roc_auc"] = roc_auc_score(y_val, preds_proba[:, 1])
-#     outputs["features"] = initial_importances
-#     return outputs
-
 
 def test_with_catboost_crossval(
     X: pl.DataFrame,
@@ -152,14 +92,14 @@ def make_aggregations(
                 df_to_agg.group_by(pl.col(id)).agg(pl.col(col).mean().suffix("_mean")),
                 on=id,
                 how="left",
-                suffix=join_suffix,
+                suffix=join_suffix if join_suffix is not None else "",
             )
         if "sum" in aggregations:
             original_df = original_df.join(
                 df_to_agg.group_by(pl.col(id)).agg(pl.col(col).sum().suffix("_sum")),
                 on=id,
                 how="left",
-                suffix=join_suffix,
+                suffix=join_suffix if join_suffix is not None else "",
             )
 
         if "min" in aggregations:
@@ -167,7 +107,7 @@ def make_aggregations(
                 df_to_agg.group_by(pl.col(id)).agg(pl.col(col).min().suffix("_min")),
                 on=id,
                 how="left",
-                suffix=join_suffix,
+                suffix=join_suffix if join_suffix is not None else "",
             )
 
         if "max" in aggregations:
@@ -175,7 +115,7 @@ def make_aggregations(
                 df_to_agg.group_by(pl.col(id)).agg(pl.col(col).max().suffix("_max")),
                 on=id,
                 how="left",
-                suffix=join_suffix,
+                suffix=join_suffix if join_suffix is not None else "",
             )
 
         if "std" in aggregations:
@@ -183,7 +123,7 @@ def make_aggregations(
                 df_to_agg.group_by(pl.col(id)).agg(pl.col(col).std().suffix("_std")),
                 on=id,
                 how="left",
-                suffix=join_suffix,
+                suffix=join_suffix if join_suffix is not None else "",
             )
 
         if "mode" in aggregations:
@@ -193,7 +133,7 @@ def make_aggregations(
                 ),
                 on=id,
                 how="left",
-                suffix=join_suffix,
+                suffix=join_suffix if join_suffix is not None else "",
             )
     return original_df
 
