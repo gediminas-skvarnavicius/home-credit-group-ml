3d3da7672630acf964e480d11114cc4b85564d98
Gediminas Skvarnavicius <gediminas.skvarnavicius@gmail.com>
2023-11-07 13:54:11 +0200

    consumer loan problem  model2 features

--- auxiliary/statistics.py
+++ auxiliary/statistics.py
@@ -3,6 +3,8 @@ from itertools import combinations
 import numpy as np
 from multiprocessing import Pool
 import polars as pl
+import scipy.stats as stats
+from typing import Optional, Tuple
 from multiprocessing import get_context
 import networkx
 
@@ -59,7 +61,7 @@ def get_correlation_pairs(
         ~(pl.col("correlation").is_between(min_threshold, max_threshold))
     )
     results = {}
-    results["pairs"] = "correlation_df"
+    results["pairs"] = correlation_df
     results["clusters"] = []
     graph = networkx.Graph()
     graph.add_edges_from(correlation_df["features"].to_list())
@@ -79,3 +81,119 @@ def calc_vif(X):
     )
 
     return vif.sort("VIF", descending=True)
+
+
+def kruskal_polars(
+    data: pl.DataFrame,
+    class_col: str,
+    var_col: str,
+    sample_size: Optional[int] = None,
+    seed: int = 1,
+    alpha: float = 0.05,
+) -> Tuple[float, float]:
+    """
+    Perform the Kruskal-Wallis test on a Polars DataFrame.
+
+    Args:
+        data (pl.DataFrame): The Polars DataFrame containing the data.
+        class_col (str): The name of the column containing class or group
+        information.
+        var_col (str): The name of the column containing the
+        variable of interest.
+        sample_size (int, optional): The sample size for each class.
+        If None, use all available data for each class.
+        seed (int): Seed for random sampling (if sample_size is provided).
+
+    Returns:
+        Tuple[float, float]: The Kruskal-Wallis H-statistic
+        and the associated p-value.
+    """
+    # Get unique class values and sort them
+    unique_classes = data[class_col].unique().sort()
+
+    samples = []
+    # Sample and collect data for each class
+    for class_val in unique_classes:
+        class_data = (
+            data.filter(data[class_col] == class_val)
+            .select(
+                var_col,
+            )
+            .drop_nulls()
+        )
+        if sample_size is not None:
+            class_data = class_data.sample(sample_size, seed=seed)
+        samples.append(class_data[var_col])
+
+    # Perform the Kruskal-Wallis test
+    result = {}
+    result["statistic"], result["p_value"] = stats.kruskal(*samples)
+    if result["p_value"] < alpha:
+        result["message"] = (
+            f"Reject the null hypothesis: There are significant "
+            f"differences in '{var_col}' between groups in '{class_col}'."
+        )
+    else:
+        result["message"] = (
+            f"Fail to reject the null hypothesis: There are no "
+            f"significant differences in '{var_col}' between groups "
+            f"in '{class_col}'."
+        )
+    return result
+
+
+def chi_squared_polars(
+    data: pl.DataFrame, class_col: str, var_col: str, alpha: float = 0.05
+):
+    """
+    Perform a chi-squared test of independence on a Polars DataFrame.
+
+    Args:
+        data (pl.DataFrame): The Polars DataFrame containing the data.
+        class_col (str): The name of the column containing class or group
+        information.
+        var_col (str): The name of the column to be used for the test
+        (e.g., "disbursement_method").
+        alpha (float): The significance level for hypothesis testing.
+
+    Returns:
+        dict: A dictionary containing the p-value, test statistic,
+        and result message.
+    """
+    data = data.select([class_col, var_col]).drop_nulls()
+    contingency_table = data[var_col].value_counts()
+    for cat in data[class_col].unique():
+        contingency_table = contingency_table.join(
+            data.filter(data[class_col] == cat)[var_col].value_counts(),
+            on=var_col,
+            how="left",
+            suffix=f"_{cat}",
+        )
+
+    contingency_table = contingency_table.drop(columns=["count", var_col])
+    # null_cols = [
+    #     col
+    #     for col in contingency_table.columns
+    #     if contingency_table[col].null_count() > 0
+    # ]
+    # contingency_table = contingency_table.drop(columns=null_cols)
+    contingency_table = contingency_table.drop_nulls()
+    test = stats.chi2_contingency(contingency_table)
+
+    result = {}
+    result["p_value"] = test.pvalue
+    result["statistic"] = test.statistic
+
+    if result["p_value"] < alpha:
+        result["message"] = (
+            f"Reject the null hypothesis: There are significant "
+            f"differences in '{var_col}' between groups in '{class_col}'."
+        )
+    else:
+        result["message"] = (
+            f"Fail to reject the null hypothesis: There are no "
+            f"significant differences in '{var_col}' between groups "
+            f"in '{class_col}'."
+        )
+
+    return result
